36d9483a1ee042a57749ec4affadc888c2af128f
ingemar <ingemar-fei@outlook.com>
2025-07-30 12:03:42 +0800

    done base32,base32hex,base64,base64urlsafe enconding and testset

--- basefamily.py
+++ basefamily.py
@@ -3,13 +3,57 @@ import base_dicts
 # rfc4648 - Base16, Base32, and Base64
 # https://www.rfc-editor.org/rfc/rfc4648
 
+base_info = {
+    'base16':{
+        'diction':base_dicts.base16_dict,
+        'digit':4,
+        },
+    'base32':{
+        'diction':base_dicts.base32_dict,
+        'digit':5,
+        },
+    'base32hex':{
+        'diction':base_dicts.base32hex_dict,
+        'digit':5,
+        },
+    'base64':{
+        'diction':base_dicts.base64_dict,
+        'digit':6,
+        },
+    'base64urlsafe':{
+        'diction':base_dicts.base64urlsafe_dict,
+        'digit':6,
+        },
+}
 #bits with value zero are added (on the right) to form an integral number of n-bit groups.  
 #Padding at the end of the data is performed using the padding character latter.
 def count_padding(hex_string:str,n:int):
-    num_padding = n - (len(hex_string) // n)
+    '''
+    cut msg into gorups that can transform wholy,like the 5 and 8 has the least common multiple number 40, 
+    which means every 5bytes can change into 8bytes base32 result and so as base64, every 3bytes input get 4bytes result.
+    the pad character here make sure reult length is as a norlmal result length.
+    i.g., in Base32, 1 byte input get 2 byte output, so it need 6 pads to get 8bytes.
+    '''
+    quantum = math.lcm(8, n)
+    num_padding = int(quantum/n) - math.ceil((len(hex_string) % quantum)/n) if (len(hex_string) % quantum)>0 else 0
+    res = hex_string + '0'* (( n - len(hex_string)%n) if  len(hex_string)%n>0 else 0) 
+    print(f" count pad: {num_padding}, \n fill result : {res}")
     return ( 
-        hex_string.zfill(len(hex_string)+num_padding), num_padding,
+        res , num_padding,
     )
+
+# get the hex_string of given string
+# the hex_string of hello : 0110100001100101011011000110110001101111 (40)
+def str_hexstring(msg)->str:
+    if type(msg)==bytes:
+        msg_ascii = msg.hex()
+    else:
+        msg_ascii = msg.encode(encoding='utf-8',errors='ignore').hex()
+    hex_string =''
+    for c in msg_ascii:
+        hex_string += bin(int(c,16))[2:].zfill(4)
+    print(f'the hex_string of {msg} :',hex_string,f'({len(hex_string)})')
+    return hex_string
 
 #split the given string into a list of fix_length substring
 #split 0110100001100101011011000110110001101111, eahc 4 bits : 
@@ -19,39 +63,42 @@ def split_fixed(s: str, n: int):
     print(f'split {s}, eahc {n} bits : \n{res}({len(res)})')
     return res
 
-# get the hex_string of given string
-# the hex_string of hello : 0110100001100101011011000110110001101111 (40)
-def str_hexstring(msg:str)->str:
-    hex_string =''
-    msg_ascii = msg.encode(encoding='ascii',errors='ignore').hex()
-    for c in msg_ascii:
-        hex_string += bin(int(c,16))[2:].zfill(4)
-    print(f'the hex_string of {msg} :',hex_string,f'({len(hex_string)})')
-    return hex_string
-
 def lookup_base_dict(hexstr_list,algorithm,num_padding):
-    dicts = {
-        'base16':base_dicts.base16_dict
-    }
+    algo_dict = base_info[algorithm]['diction']
     int_list = list(map(lambda t:int(t,2), hexstr_list))
     #print(int_list)
-    char_list = list(map(lambda t:dicts[algorithm][t], int_list))
+    char_list = list(map(lambda t:algo_dict[t], int_list))
     #print(char_list)
     raw_res = ''.join(char_list)
-    print(f'before adding padding: {raw_res}({len(raw_res)})')
-    padding = (dicts[algorithm]['padding'] * num_padding) if num_padding>0 else ''
+    print(f'before adding pad: {raw_res}({len(raw_res)})')
+    padding = (algo_dict['pad'] * num_padding) if num_padding>0 else ''
     res = raw_res + padding
-    print(f'the finnaly string : {res}')
+    print(f'--- the finnaly string : {res} \n')
     return res
 
 
 # the standard is using ASCII code
-def base16(msg:str)->str:
-    hex_string,num_padding = count_padding(str_hexstring(msg),4)
-    res_list = split_fixed(hex_string,4)
-    res = lookup_base_dict(res_list,'base16',num_padding) 
+def base_family(msg:str,algorithm:str)->str:
+    hex_string,num_padding = count_padding(str_hexstring(msg),base_info[algorithm]['digit'])
+    res_list = split_fixed(hex_string,base_info[algorithm]['digit'])
+    res = lookup_base_dict(res_list,algorithm,num_padding) 
     return res
+
+def base16(msg:str)->str:
+    return base_family(msg,'base16')
+def base32(msg:str)->str:
+    return base_family(msg,'base32')
+def base32hex(msg:str)->str:
+    return base_family(msg,'base32hex')
+def base64(msg:str)->str:
+    return base_family(msg,'base64')
+def base64urlsafe(msg:str)->str:
+    return base_family(msg,'base64urlsafe')
 
 if __name__=='__main__':
     #str_hexstring('hello')
-    print(base16('foobar'))
+    print(base16(b'foobar'))
+    print(base32('foobar'))
+    print(base32hex('foobar'))
+    print(base64('foobar'))
+    print(base64urlsafe('foobar'))
---
+++ test_basefamily.py
@@ -0,0 +1,72 @@
+import unittest
+import basefamily
+
+class TestBaseFamily(unittest.TestCase):
+    testset = {
+        'base16':[
+            ("f","66"),
+            ("fo","666F"),
+            ("foo","666F6F"),
+            ("foob","666F6F62"),
+            ("fooba","666F6F6261"),
+            ("foobar","666F6F626172"),
+        ],
+        'base32':[
+            ("",""),
+            ("f","MY======"),
+            ("fo","MZXQ===="),
+            ("foo","MZXW6==="),
+            ("foob","MZXW6YQ="),
+            ("fooba","MZXW6YTB"),
+            ("foobar","MZXW6YTBOI======"),
+        ],
+        'base32hex':[
+            ("",""),
+            ("f","CO======"),
+            ("fo","CPNG===="),
+            ("foo","CPNMU==="),
+            ("foob","CPNMUOG="),
+            ("fooba","CPNMUOJ1"),
+            ("foobar","CPNMUOJ1E8======"),
+        ],
+        'base64':[
+            ("",""),
+            ("f","Zg=="),
+            ("fo","Zm8="),
+            ("foo","Zm9v"),
+            ("foob","Zm9vYg=="),
+            ("fooba","Zm9vYmE="),
+            ("foobar","Zm9vYmFy"),
+        ],
+        'base64urlsafe' : [
+            (b'f','Zg=='),
+            (b'fo','Zm8='),
+            (b'foo','Zm9v'),
+            (b'foob','Zm9vYg=='),
+            (b'fooba','Zm9vYmE='),
+            (b'foobar','Zm9vYmFy'),
+            (b'\x00\x01\x02','AAEC'),
+            (b'\xFF\xEF\xFE','_-_-'),
+            (b'\xDE\xAD\xBE\xEF','3q2-7w=='),
+            (b'\x99\x88\x77\x66\x55\x44\x33\x22\x11','mYh3ZlVEMyIR'),
+        ]
+    }
+
+    def test_base16(self):
+        for test,res in self.testset['base16']:
+            self.assertEqual(basefamily.base16(test),res)
+    def test_base32(self):
+        for test,res in self.testset['base32']:
+            self.assertEqual(basefamily.base32(test),res)
+    def test_base32hex(self):
+        for test,res in self.testset['base32hex']:
+            self.assertEqual(basefamily.base32hex(test),res)
+    def test_base64(self):
+        for test,res in self.testset['base64']:
+            self.assertEqual(basefamily.base64(test),res)
+    def test_base64urlsafe(self):
+        for test,res in self.testset['base64urlsafe']:
+            self.assertEqual(basefamily.base64urlsafe(test),res)
+
+if __name__ == '__main__':
+    unittest.main()
